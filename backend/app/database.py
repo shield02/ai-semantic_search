9c7fae37aac7203819787528ad9a9f96c69e3b14
otuekong <myshield02@gmail.com>
2024-10-29 12:50:20 +0100

    initial interaction with pinecone database

---
+++ backend/app/database.py
@@ -0,0 +1,10 @@
+from config import index
+from models.encoder import encode_text
+
+def add_text(text_id: str, text: str):
+    vector = encode_text(text)
+    index.upsert([(text_id, vector)])
+
+def search_query(query: str, top_k: int = 5):
+    query_vector = encode_text(query)
+    return index.query(query_vector, top_k=top_k)
