b2aeb59223e75ca3007efecaf9b2e77708e55716
otuekong <myshield02@gmail.com>
2024-10-29 14:00:56 +0100

    initial component for showing results

---
+++ frontend/src/components/ResultsComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-if="results.length">
+    <h3>Results:</h3>
+    <div v-for="result in results" :key="result.id">
+      <p>{{ result.text }}</p>
+    </div>
+  </div>
+  <p v-else>No results found</p>
+</template>
+
+<script>
+import { useSearchStore } from '../stores/searchStore';
+import { computed } from 'vue';
+
+export default {
+  setup() {
+    const searchStore = useSearchStore();
+    const results = computed(() => searchStore.results);
+
+    return { results };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
