9a22cd1e0527fcbdc786422676f79b534a067fa5
otuekong <myshield02@gmail.com>
2024-10-29 15:16:51 +0100

    commented out pinecone init line, added api_key and host params to index

--- backend/app/config.py
+++ backend/app/config.py
@@ -8,5 +8,5 @@ PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT")
 INDEX_NAME = os.getenv("INDEX_NAME")
 
-pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
-index = pinecone.Index(name=INDEX_NAME)
+# pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
+index = pinecone.Index(name=INDEX_NAME, api_key=PINECONE_API_KEY, host=PINECONE_ENVIRONMENT)
