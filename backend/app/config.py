7e99a5706a9a2c042f781bb16273364df10c8b9f
otuekong <myshield02@gmail.com>
2024-10-29 12:48:31 +0100

    initial configuration for pinecone

---
+++ backend/app/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+import pinecone
+
+load_dotenv()
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT")
+INDEX_NAME = os.getenv("INDEX_NAME")
+
+pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
+index = pinecone.Index(name=INDEX_NAME)
